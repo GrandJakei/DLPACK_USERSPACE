36a1c7ff0848118a518432151519fcb628b968b6
jakei <2435207533@qq.com>
2022-10-14 21:28:41 -0700

    pload syscall version

--- src/pload.c
+++ src/pload.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #define PATH_LOAD "/sys/kernel/security/dlpack/loadRules"
 #define PATH_CLEAN "/sys/kernel/security/dlpack/changeRules"
 #define PATH_POLICY "result"
@@ -9,42 +13,39 @@
 //#define PATH_POLICY "result"
 #define CHAR_MAX_LENGTH 256
 char to_deal[CHAR_MAX_LENGTH];  // 待处理的一行文本
-char *to_kernel;
 
 int main() {
-    FILE *f_policy = fopen(PATH_POLICY, "r");
-    FILE *f_load = fopen(PATH_LOAD, "w");
-    FILE *f_clean = fopen(PATH_CLEAN, "w");
+    int f_policy = open(PATH_POLICY, O_RDONLY);
+    int f_load = open(PATH_LOAD, O_RDWR);
+    int f_clean = open(PATH_CLEAN, O_RDWR);
 
-    if(f_policy == NULL){
+    if(!f_policy){
         printf("error : fail to open profile f_policy in %s! please run pcheck first.\n", PATH_POLICY);
         goto error;
     }
-    if(f_load == NULL){
+    if(!f_load){
         printf("error : fail to write in %s! may not have enough permission.\n", PATH_LOAD);
         goto error;
     }
-    if(f_clean == NULL){
+    if(!f_clean){
         printf("error : fail to write in %s! may not have enough permission.\n", PATH_CLEAN);
         goto error;
     }
 
     // 清空内核策略
     int index, pid;
-    fputs("clean", f_clean);
+    write(f_clean, "clean", 6);
 
     // 
-    to_kernel = malloc(sizeof(char) * CHAR_MAX_LENGTH);
-    while (fgets(to_deal, CHAR_MAX_LENGTH, f_policy) != NULL){
-        to_kernel[strlen(to_kernel) - 1] = '\0';
-    	fputs(to_kernel, f_load);
+    while (read(f_policy, to_deal, CHAR_MAX_LENGTH) > 0){
+        to_deal[strlen(to_deal) - 1] = '\0';
+    	write(f_load, to_deal, strlen(to_deal));
+    	printf("to_kernel : %s \n", to_deal);
     }
-    printf("to_kernel : %s \n", to_kernel);
-    fputs(to_kernel, f_load);
     printf("policy loading done successfully! \n");
-    fclose(f_load);
-    fclose(f_policy);
-    fclose(f_clean);
+    close(f_load);
+    close(f_policy);
+    close(f_clean);
 error:
     return 0;
 }
